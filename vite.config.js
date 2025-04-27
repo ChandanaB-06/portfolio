22f3ed179027a6bc22929d3da650e531306ede7b
ChandanaB-06 <chandanabblr@gmail.com>
2025-04-27 18:59:20 +0530

    first comment

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
-    base: process.env.VITE_BASE_PATH || "/portfolio"
+    base: process.env.VITE_BASE_PATH || "/Cportfolio"
 })
